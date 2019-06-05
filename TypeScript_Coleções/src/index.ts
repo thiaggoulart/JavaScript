29508173972bb4347813934910de2c29ccd70e30
Thiago Goulart <thiaggoulart@gmail.com>
2019-06-05 17:25:43 -0300

    Aula 05/06/2019

--- TypeScript_Coleções/src/index.ts
+++ TypeScript_Coleções/src/index.ts
@@ -1,5 +1,24 @@
-let mapa: Map<string, string> = new Map();
+let mapa: Map<string, string>;
+mapa = new Map();
 mapa.set('RS','Rio Grande do Sul');
 mapa.set('SC',"Santa Catarina");
+
 console.log(mapa.size);
 console.log(mapa.get('RJ'));
+
+for(let entrada of mapa){
+    console.log(entrada[0]);
+    console.log(entrada[1]);
+}
+for(let sigla of mapa.keys()){
+    console.log(sigla);
+}
+for(let estados of mapa.values()){
+    console.log(estados);
+}
+
+function logMapElements(values:string, key:string, map:Map<string,string>){
+    console.log(`map.get('${key}') = ${values}`);
+}
+
+mapa.forEach(logMapElements);
