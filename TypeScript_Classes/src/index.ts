c586cafdbcf2013416825b0bc17b1b03a85b9093
Thiago Goulart <thiaggoulart@gmail.com>
2019-06-07 16:22:25 -0300

    Interface + exemplo json

--- TypeScript_Classes/src/index.ts
+++ TypeScript_Classes/src/index.ts
@@ -6,7 +6,7 @@ class Pessoa {
         this.nome = n;
         this.idade = i;
     }
-}
+}/*
 
 let p1: Pessoa;
 p1 = new Pessoa('John Doe', 22);
@@ -105,3 +105,90 @@ console.log(prod1.toString());
 let prod2 = new ProdutoPerecivel('prod2', 100, new Date(2019,12,31));
 console.log(prod2);
 console.log(prod2.toString());
+
+abstract class FiguraBidimensional {
+    constructor(
+        private _centrox: number,
+        private _centroy: number
+    ){}
+
+    get x(): number {
+        return this._centrox;
+    }
+
+    get y(): number {
+        return this._centroy;
+    }
+
+    abstract area(): number;
+}
+
+class Circulo extends FiguraBidimensional {
+    constructor(
+        centrox: number,
+        centroy: number,
+        private _raio: number){
+            super(centrox, centroy);
+        }
+    
+    get raio(): number{
+        return this._raio;
+    }
+
+    area(): number {
+       return Math.PI * this._raio ** 2;
+    }
+}
+
+let fig1: FiguraBidimensional = new Circulo(1,1,3);
+console.log(fig1);
+console.log(fig1.area());
+console.log(fig1.x);
+console.log((<Circulo>fig1).raio);
+
+let  pessoa = {
+    nome: 'john Doe',
+    idade: 22
+};
+
+let {nome, idade} = pessoa;
+console.log(nome);
+console.log(idade);
+
+let {x,y} = fig1;
+console.log(x);
+console.log(y);
+
+interface Desenhavel {
+    x: number,
+    y: number
+}
+
+function desenhar(d: Desenhavel) {
+    console.log(`x=${d.x} y=${d.y}`);
+}
+
+desenhar(fig1);
+
+interface Predicado <T> {
+    (item:T) : boolean;
+}
+
+function filtrar<T>(array:T[], filtro: Predicado<T>):T[] {
+    let resultado:T[] = [];
+    for (let i=0; i <array.length; i++){
+        if(filtro(array[i])){
+            resultado.push(array[i]);
+        }
+    }
+    return resultado;
+}
+
+console.log(filtrar([0,10,1,3,2], i =>i%2===0));*/
+
+let umaPessoa = new Pessoa('John Doe', 22);
+let json = JSON.stringify(umaPessoa);
+console.log(json);
+let jsonstring = '{"nome":"John Doe", "idade": 22}';
+let obj = JSON.parse(jsonstring);
+console.log(obj);
