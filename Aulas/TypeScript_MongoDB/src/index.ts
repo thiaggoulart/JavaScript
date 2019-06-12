989000e2cbf6ab9bda929ba7506699a5db17f6fe
Thiago Goulart <thiaggoulart@gmail.com>
2019-06-12 17:26:52 -0300

    Repositório professor

--- Aulas/TypeScript_MongoDB/src/index.ts
+++ Aulas/TypeScript_MongoDB/src/index.ts
@@ -13,11 +13,21 @@ async function Main() {
         console.log(`Inserido: ${resultIns.insertedId}`);*/
 
         //Consultar documentos
-        const cursor: Cursor = banco. collection('pessoas').find({idade:{$gt:22}});
+        /*const cursor: Cursor = banco. collection('pessoas').find({idade:{$gt:25}});
         while(await cursor.hasNext()){
             const documento = await cursor.next();
             console.log(documento.nome);
-        }
+        }*/
+
+        //Alterar documento
+        /*const resultAlt = await banco.collection('pessoas').updateOne({nome:'Teste'},{$set:{idade:50}});
+        console.log(`Alteração: ${resultAlt.modifiedCount}`);*/
+
+        //Deletar documento
+        const resultRem = await banco.collection('pessoas').deleteOne({nome:'Teste'});
+        console.log(`Remoção:${resultRem.deletedCount}`);
+        
+
         await cliente.close();
     } catch (erro) {
         console.log('Erro de acesso ao BD: ');
