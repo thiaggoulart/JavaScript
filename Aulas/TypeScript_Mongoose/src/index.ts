db474ae24a1ce094f216db77714f6afb893179ce
Thiago Goulart <thiaggoulart@gmail.com>
2019-06-13 14:47:20 -0300

    Aula mongoose

---
+++ Aulas/TypeScript_Mongoose/src/index.ts
@@ -0,0 +1,23 @@
+import {connect, Schema} from 'mongoose';
+
+async function Main() {
+    try{
+        const url = 'mongodb://localhost:27017/testemongoose';
+        const cliente = await connect(url,{useNewUrlParser:true});
+        console.log('Conectado com sucesso!');
+       
+        const pessoaEsquema = new Schema({
+            nome: {type: String, required: true, minlength: 1, maxlength: 50},
+            idade: {type: Number, required: true, minlength: 0}
+        });
+
+        if(cliente && cliente.connection) {
+            cliente.connection.close();
+        }
+    } catch(error){
+        console.log('Erro: ');
+        console.log(error);
+    }
+}
+
+Main();
