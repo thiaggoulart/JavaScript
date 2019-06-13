cee31ec661607b76efbfa1da9d224ece4657acda
Thiago Goulart <thiaggoulart@gmail.com>
2019-06-13 16:38:42 -0300

    Aula Mongoose

--- Aulas/TypeScript_Mongoose/src/index.ts
+++ Aulas/TypeScript_Mongoose/src/index.ts
@@ -6,11 +6,41 @@ async function Main() {
         const cliente = await connect(url,{useNewUrlParser:true});
         console.log('Conectado com sucesso!');
        
-        const pessoaEsquema = new Schema({
+        //Definir Schema e Model
+        const pessoaEsquema = new Schema({ //linha do bonco que irá mexer
             nome: {type: String, required: true, minlength: 1, maxlength: 50},
             idade: {type: Number, required: true, minlength: 0}
         });
-        const pessoaModelo = model('PessoaModel', pessoaEsquema, 'pessoas');
+        const pessoaModelo = model('PessoaModel', pessoaEsquema, 'pessoas'); //contem os métodos do banco
+
+        /*
+        //Inserir documento
+        const pessoaDocumento = new pessoaModelo({nome: 'John Doe', idade:22});
+        const documentoInserido = await pessoaDocumento.save(); //insere um documento no banco
+        console.log(`Inserido`);
+        console.log(documentoInserido);
+        */
+
+        /*
+        //Consultar documentos
+        //const consulta = pessoaModelo.find(); //consulta todo mundo
+        const consulta = pessoaModelo.where('idade').lt(30).sort('idade'); //consulta pessas com idade menor que 30
+        const resultado = await consulta.exec();
+        console.log(resultado);
+        */
+
+        /*
+        //Alterar documento
+        const documentoPessoa = await pessoaModelo.findById('5d02905a9bbb611fe88afa16').exec();
+        console.log(documentoPessoa);
+        documentoPessoa!.set('idade',23); //muda idade para 23 *obs: tem que falar para ele que pode ser null
+        console.log(documentoPessoa);
+        await documentoPessoa!.save();
+        */
+
+        //Remover documento
+        const documentoPessoa = await pessoaModelo.findById('5d02905a9bbb611fe88afa16');
+        await documentoPessoa!.remove(); //remove um documento do banco
 
         if(cliente && cliente.connection) {
             cliente.connection.close();
